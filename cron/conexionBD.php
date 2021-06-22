b8831991787286c3e0b5a7dd891bde09cf440c60
KMBAsesor61 <KMBAsesor61@KMB.UIO>
2021-06-22 11:03:45 -0500

    insercion correcta y simple de archivo json a mysql en el archivo  conexionBD

--- cron/conexionBD.php
+++ cron/conexionBD.php
@@ -14,22 +14,36 @@ echo "Connecion exitosa ";
 
 /* optencion del contenido json */
  $jsonCont = file_get_contents('datos.json');
+
+ 
 //decodificacion del conetnido json y convetirlo en un array en php dentro de una variable  */ 
 $content = json_decode($jsonCont, true);
 
+foreach($content as $row) {
+
+    $nombre = $row['nombre'];
+    $categoria = $row['categoria'];
+    $precio = $row['precio'];
+    $cantidad_vendidos = $row['cantidad_vendidos'];
+    $en_almacen = $row['en_almacen'];
+    $fecha_alta = $row['fecha_alta'];
+
+    $query = "INSERT INTO resumen_productos ( nombre, categoria, precio, cantidad_vendidos, en_almacen, fecha_alta) VALUES('$nombre', '$categoria', '$precio', '$cantidad_vendidos', '$en_almacen', '$fecha_alta')";
+    mysqli_query($conn,$query);
+}
+
+
 /* busqueda de los datos a la matriz  */
 
-$nombre = $content['nombre'];
-$categoria = $content['categoria'];
-$precio = $content['precio'];
-$cantidad_vendidos = $content['cantidad_vendidos'];
-$en_almacen = $content['en_almacen'];
-$fecha_alta = $content['fecha_alta'];
+
+
+/* prueba for each */
+
 
 /* instertar los datos dentro de la base mediante sentencias sql */
-$query = "INSERT INTO resumen_productos ( nombre, categoria, precio, cantidad_vendidos, en_almacen, fecha_alta) VALUES('$nombre', '$categoria', '$precio', '$cantidad_vendidos', '$en_almacen', '$fecha_alta')";
-if(!mysqli_query($query,$conn)) { 
- die('Error : query no ejecutado ! ' . mysqli_error());
+
+if(!mysqli_query($conn, $query)) { 
+ die('Error : query no ejecutado ! ' . mysqli_error($conn,$query));
 } else{ 
  echo "datos insertados correctamente ";
 } 
